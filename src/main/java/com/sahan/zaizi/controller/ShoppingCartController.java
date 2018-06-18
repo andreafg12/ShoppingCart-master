ef0b81e10cf094888eed3fd860fcbee6bfe82123
aflorgar <aflorgar@BOG-BP6N1N2.mshome.net>
2018-06-18 16:08:12 -0500

    Signed-off-by: aflorgar <aflorgar@BOG-BP6N1N2.mshome.net>

--- src/main/java/com/sahan/zaizi/controller/ShoppingCartController.java
+++ src/main/java/com/sahan/zaizi/controller/ShoppingCartController.java
@@ -3,8 +3,30 @@ package com.sahan.zaizi.controller;
 public class ShoppingCartController {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.err.println("Hola");
+
+		String requestContentAsString = "------WebKitFormBoundaryZ8HCBzhVWbUANtKM"+"\n"+
+				"Content-Disposition: form-data; name='descripcion'"+"\n\n"+"bhvbc"+"\n"+"------WebKitFormBoundaryZ8HCBzhVWbUANtKM"
+				+"\n"+"Content-Disposition: form-data; name='tipoRelacion'"+"\n\n"+"CLIENTE"+"\n"+"------WebKitFormBoundaryZ8HCBzhVWbUANtKM"+
+				"\n"+"Content-Disposition: form-data; name='uploadfile'; filename='diagrama.pdf'"+"\n"+"Content-Type: application/pdf"+"\n\n"+
+				"%PDF-1.7";
+		int endOfHeaderIndex = 0, beginOfHeaderIndex = 0;
+		String line, name = "tipoRelacion", tipo = "value";
+		while (endOfHeaderIndex < requestContentAsString.length() && beginOfHeaderIndex > -1 && endOfHeaderIndex > -1) {
+			beginOfHeaderIndex = requestContentAsString.indexOf("------WebKitFormBoundaryZ8HCBzhVWbUANtKM", beginOfHeaderIndex);
+			endOfHeaderIndex = requestContentAsString.indexOf("\n\n", beginOfHeaderIndex);
+			line = requestContentAsString.substring(beginOfHeaderIndex, endOfHeaderIndex);
+			System.out.println(beginOfHeaderIndex+".."+endOfHeaderIndex+line);
+			if(line.contains(name)) {
+				if("value".equalsIgnoreCase(tipo)) {
+					//Obtiene el valor del campo
+					System.out.println(requestContentAsString.indexOf("------WebKitFormBoundaryZ8HCBzhVWbUANtKM", endOfHeaderIndex));
+					line = requestContentAsString.substring(endOfHeaderIndex, requestContentAsString.indexOf("------WebKitFormBoundaryZ8HCBzhVWbUANtKM", endOfHeaderIndex));
+				}
+				System.out.println(line);
+				break;
+			}
+			beginOfHeaderIndex = endOfHeaderIndex + 1;
+		}
 	}
 
 }
